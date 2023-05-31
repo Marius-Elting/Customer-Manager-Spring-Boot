ef632801c90ebaac6e02821f2248f79dc3c7dc22
Marius-Elting <Marius.Supercode@gmail.com>
2023-05-31 11:00:58 +0200

    first little API

--- src/main/java/com/marius/Main.java
+++ src/main/java/com/marius/Main.java
@@ -2,10 +2,18 @@ package com.marius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String greet() {
+        return "<h1>Hello</h1>";
     }
 }
