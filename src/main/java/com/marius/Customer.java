53c20905198e8410d363d96e3e7e2f450d224485
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-02 15:13:00 +0530

    created Customer Entity

--- src/main/java/com/marius/Customer.java
+++ src/main/java/com/marius/Customer.java
@@ -1,6 +1,16 @@
 package com.marius;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
     private String name;
     private String email;
