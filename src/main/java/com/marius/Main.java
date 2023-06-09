ad31fa2a8cb2719ef70cf6b7fd312f5a03b10238
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-09 13:54:35 +0200

    added delete & update Method

--- src/main/java/com/marius/Main.java
+++ src/main/java/com/marius/Main.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 // import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +59,21 @@ public class Main {
         customer.setName(request.name());
         customer.setEmail(request.email());
         customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customer.setId(id);
         customerRepository.save(customer);
     }
 
