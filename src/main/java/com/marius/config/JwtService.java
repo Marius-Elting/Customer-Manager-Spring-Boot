e1dadb46c4b2e8e795710d1a0436821b34fdc90a
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-15 20:19:53 +0200

    implemented extreactAllClaims & getSigningKey in JWTService

--- src/main/java/com/marius/config/JwtService.java
+++ src/main/java/com/marius/config/JwtService.java
@@ -1,11 +1,34 @@
 package com.marius.config;
 
+import java.security.Key;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "JKABSFKLBA9F8A89FHALDFBAF8Q39R883JKADFKH381290U345";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
