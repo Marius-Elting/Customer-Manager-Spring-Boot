3842a7f95982836fc7caa77cd9a25290b19b1137
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-15 14:30:31 +0200

    added JwtAuthenticationConfig class

---
+++ src/main/java/com/marius/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.marius.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
