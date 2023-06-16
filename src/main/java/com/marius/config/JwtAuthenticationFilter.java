59138c2b56effa8ff72a2e385210257f0f2d5fe3
Marius-Elting <Marius.Supercode@gmail.com>
2023-06-16 21:32:13 +0200

    finished JwtAuthenticationFilter

--- src/main/java/com/marius/config/JwtAuthenticationFilter.java
+++ src/main/java/com/marius/config/JwtAuthenticationFilter.java
@@ -2,9 +2,11 @@ package com.marius.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -38,9 +40,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadByUsername(userEmail);
+            // if we get the userEmail and the User is not authenticatet yet we got the
+            // Userdata from the Datanase
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            // check if the user and the token is valid
+            if (jwtService.isTokenValid(userEmail, userDetails)) {
+                // create UsernamePasswordAuthenticationToken
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // update the authentication token
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
-        ;
+        filterChain.doFilter(request, response);
 
     }
 
